85e93fb23fdf2a216b8e284d85ce5574ec77fed9
ranixhirani-backup <hiranixrani@gmail.com>
2025-12-11 16:36:53 +0530

    feat: added service  layer for course

--- repository/course.go
+++ repository/course.go
@@ -4,11 +4,26 @@ import(
 	"studentProject/models"
 )
 type CourseRepository interface{
-	Create(course models.Course) (int, error)
+	Create(course models.Course) (int, error) //Any struct that has a method with the exact same signature automatically implements this interface.
 }
 type courseRepository struct{
-	DB *sql.DB
+	DB *sql.DB //pointer to db connection
 }
 func NewCourseRepository(db *sql.DB) CourseRepository{
-	return &courseRepository{DB: db}
+	return &courseRepository{DB: db}  //creates your repository and returns a pointer to it so other parts of your program can use it.
+}
+
+func (r *courseRepository) Create(course models.Course) (int, error){ //function of courseRepository struct type that immplements Create method which makes it of CourseRepository Interface
+	var id int														 //this function takes course from model and input parameter and returns either int or error			
+	query:= `
+		INSERT INTO course (course_name, credits, description)
+		VALUES($1, $2, $2)
+		RETURNING student_id
+	`
+	err:= r.DB.QueryRow(query,
+	course.CourseName,
+	course.Credits,
+	course.Description,
+	).Scan(&id)
+	return id,err
 }
---
+++ services/course.go
@@ -0,0 +1,18 @@
+package services
+import(
+	"studentProject/models"
+	"studentProject/repository"
+)
+type CourseService interface{ //interface of CourseService, any struct that would implement CreateCourse with the given signature will automatically implement this interface
+	CreateCourse(course models.Course) (int, error)
+}
+type courseService struct{ //courseService is the concrete implementation of the CourseService interface.
+							//It depends on a CourseRepository, which is injected into it.
+	repo repository.CourseRepository 
+}
+func NewCourseService(repo repository.CourseRepository) CourseService{ //This is a constructor function.It creates a new courseService and returns it as a CourseService interface.We return a pointer because the methods have pointer receivers
+		return &courseService{repo:repo}
+}
+func (s *courseService) CreateCourse(course models.Course) (int, error){ //implementation of the method defined in the interface
+	return s.repo.Create(course) 
+}
